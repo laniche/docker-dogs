1e30c2ec573298151ee3db804031b29b077a1397
Thierry Lagasse <epagneul@dogstudio.be>
2016-09-29 23:31:24 +0200

    Add database connection test

--- dev/public/index.php
+++ dev/public/index.php
@@ -1,19 +1,18 @@
 <?php
-/**
- * index.php
- *
- * @author epagneul
- * @copyright  Copyright (c) 2016 Dogstudio.be
- * @since Jan 2016
- *
- * @package     docker
- * @subpackage
- */
+try {
+    $pdo = new PDO("mysql:host=database;dbname=docker_dev", 'root', 'docker');
+    $stmt = $pdo->query('SELECT * FROM dockerlabs');
+    $datas = $stmt ? $stmt->fetchAll(PDO::FETCH_ASSOC) : null;
 
-$now = new DateTime();
-echo '<pre>', print_r([
-    $now->format('c'),
-    $_SERVER
-], true), '</pre>'; exit;
-
-
+    echo '<pre>', print_r([
+        'php_version' => phpversion(),    
+        'render_date' => new DateTime('now'),
+        'pdo_datas' => $datas,
+    ], true), '</pre>'; 
+}
+catch(PDOException $e) {
+    echo '<h1 style="color:red">Database Error</h1><p>', $e->getMessage(), '</p>', PHP_EOL;
+}
+catch(Exception $e) {
+    echo '<h1 style="color:red">Error</h1><p>', $e->getMessage(), '</p>', PHP_EOL;
+}
